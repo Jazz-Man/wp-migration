f5466bb973677dd6c7d1a82c9900605229557bbc
Vasyl Sokolyk <vsokolyk@gmail.com>
2023-06-22 00:29:35 +0300

    fix phpCsFixer Config

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -4,6 +4,6 @@ use function JazzMan\PhpCsFixerRules\phpCsFixerConfig;
 
 require_once __DIR__.'/vendor/jazzman/php-cs-fixer-rules/src/rules-config.php';
 
-$project_dir = __DIR__.'/web/app/themes/boilerplate';
+$project_dir = __DIR__.'/wp';
 
 return phpCsFixerConfig($project_dir);
