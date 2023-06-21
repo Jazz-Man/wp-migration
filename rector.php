c94e56151ba415d995500aaf666f1ff643e9afbe
Vasyl Sokolyk <vsokolyk@gmail.com>
2023-06-22 00:29:15 +0300

    enabled phpstan config

--- rector.php
+++ rector.php
@@ -8,49 +8,46 @@ use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
 
 return static function (RectorConfig $config): void {
-
-	$config->sets([
-		SetList::CODE_QUALITY,
-		SetList::CODING_STYLE,
-		SetList::TYPE_DECLARATION,
-		SetList::EARLY_RETURN,
-//		SetList::NAMING,
-//		SetList::PRIVATIZATION,
-		LevelSetList::UP_TO_PHP_82,
-	]);
-
-
-
-	$config->fileExtensions(['php']);
-	$config->importNames();
-	$config->removeUnusedImports();
-	$config->importShortClasses(false);
-	$config->disableParallel();
-	$config->cacheDirectory(__DIR__.'/cache/rector');
-
-
-	$config->paths([
-        __DIR__ . '/wp',
+    $config->sets([
+        SetList::CODE_QUALITY,
+        SetList::CODING_STYLE,
+        SetList::TYPE_DECLARATION,
+        SetList::EARLY_RETURN,
+        SetList::INSTANCEOF,
+        //		SetList::NAMING,
+        //		SetList::PRIVATIZATION,
+        LevelSetList::UP_TO_PHP_82,
     ]);
 
+    $config->fileExtensions(['php']);
+    $config->importNames();
+    $config->removeUnusedImports();
+    $config->importShortClasses(false);
+    $config->disableParallel();
+    $config->cacheDirectory(__DIR__.'/cache/rector');
+    $config->phpstanConfig(__DIR__.'/phpstan-rector.neon');
 
-	$config->skip([
-		__DIR__.'/vendor',
-		__DIR__.'/cache',
-		__DIR__.'/wp/wp-admin/css',
-		__DIR__.'/wp/wp-admin/images',
-		__DIR__.'/wp/wp-admin/js',
+    $config->paths([
+        __DIR__.'/wp',
+        __DIR__.'/old-libs',
+    ]);
 
-		__DIR__.'/wp/wp-includes/certificates',
-		__DIR__.'/wp/wp-includes/css',
-		__DIR__.'/wp/wp-includes/fonts',
-		__DIR__.'/wp/wp-includes/images',
-		__DIR__.'/wp/wp-includes/js',
-		__DIR__.'/old-libs',
-	]);
+    $config->skip([
+        __DIR__.'/vendor',
+        __DIR__.'/cache',
+        __DIR__.'/wp/wp-admin/css',
+        __DIR__.'/wp/wp-admin/images',
+        __DIR__.'/wp/wp-admin/js',
+
+        __DIR__.'/wp/wp-includes/certificates',
+        __DIR__.'/wp/wp-includes/css',
+        __DIR__.'/wp/wp-includes/fonts',
+        __DIR__.'/wp/wp-includes/images',
+        __DIR__.'/wp/wp-includes/js',
+    ]);
 
     // register a single rule
-//	$config->rule(InlineConstructorDefaultToPropertyRector::class);
+    //	$config->rule(InlineConstructorDefaultToPropertyRector::class);
 
     // define sets of rules
     //    $rectorConfig->sets([
