e8dcbac4e3d4c3a3807965e54c69c8d7fc01dbea
Vasyl Sokolyk <vsokolyk@gmail.com>
2023-07-16 00:29:25 +0300

    style(composer): update jazzman/php-cs-fixer-rules

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -6,4 +6,4 @@ require_once __DIR__.'/vendor/jazzman/php-cs-fixer-rules/src/rules-config.php';
 
 $project_dir = __DIR__.'/wp';
 
-return phpCsFixerConfig($project_dir);
+return phpCsFixerConfig( $project_dir );
